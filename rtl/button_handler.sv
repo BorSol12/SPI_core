236b3ce5910af231fe2b3252972ae48dbcf248a2
Борис Соловьев <boyan4g@gmail.com>
2025-10-04 14:38:27 +0300

    handler

--- rtl/button_handler.sv
+++ rtl/button_handler.sv
@@ -7,23 +7,26 @@ module button_handler(
     input s_rst,
     input a_rst,
 
-    output logic next_count,
-    output logic start_send
+    output logic next_count_o,
+    output logic start_send_o
   );
 
-// Logic of next_count and start_send
+logic next_count;
+logic start_send;
+
+// Logic of next_count_o and next_count_o
 always_ff @(posedge clk_100 or posedge a_rst)
     if (a_rst) begin
-        next_count <= 0;
-        start_send <= 0;
+        next_count_o <= 0;
+        next_count_o <= 0;
     end
     else if (s_rst) begin
-        next_count <= 0;
-        start_send <= 0;
+        next_count_o <= 0;
+        next_count_o <= 0;
     end
     else begin
-        next_count <= button_0;
-        start_send <= button_1;
+        next_count_o <= next_count;
+        next_count_o <= start_send;
     end
 
 sync_fd sync_fd_button_0(
